7a03c42d5415eefcbf514097b0e4dfd4d685e813
Thomas Oldham <toldham@uw.edu>
2021-04-28 10:34:30 -0700

    Move gadget-spec flags to fingerprint command

--- cmd/fingerprint.go
+++ cmd/fingerprint.go
@@ -3,13 +3,20 @@ package cmd
 import (
     "fmt"
     "github.com/polyverse/ropoly-cmd/lib/types/Fingerprint"
+    "github.com/polyverse/ropoly-cmd/lib/types/Gadget"
     "github.com/spf13/cobra"
 )
 
+const MIN_GADGET_LENGTH_DEFAULT = 0
+const MAX_GADGET_LENGTH_DEFAULT = 6
+
+var gadgetSpec Gadget.UserSpec
 var inputIsPid bool
 
 func init() {
     fingerprintCmd.PersistentFlags().BoolVarP(&inputIsPid, "pid", "p", false, "Input argument represents the PID of a running process, rather than a path to a binary file.")
+    fingerprintCmd.PersistentFlags().UintVarP(&gadgetSpec.MinSize, "gadget-min-length", "s", MIN_GADGET_LENGTH_DEFAULT, "minimum gadget length to include in fingerprint")
+    fingerprintCmd.PersistentFlags().UintVarP(&gadgetSpec.MaxSize, "gadget-max-length", "l", MAX_GADGET_LENGTH_DEFAULT, "maximum gadget length to include in fingerprint")
 
     rootCmd.AddCommand(fingerprintCmd)
 }
--- cmd/root.go
+++ cmd/root.go
@@ -2,15 +2,9 @@ package cmd
 
 import (
     "fmt"
-    "github.com/polyverse/ropoly-cmd/lib/types/Gadget"
     "github.com/spf13/cobra"
     "os"
 )
-
-const MIN_GADGET_LENGTH_DEFAULT = 0
-const MAX_GADGET_LENGTH_DEFAULT = 6
-
-var gadgetSpec Gadget.UserSpec
 
 var rootCmd = &cobra.Command{
   Use:      "./ropoly-cmd",
@@ -26,9 +20,4 @@ func Execute() {
     fmt.Fprintln(os.Stderr, err)
     os.Exit(1)
   }
-}
-
-func init() {
-    rootCmd.PersistentFlags().UintVarP(&gadgetSpec.MinSize, "gadget-min-length", "s", MIN_GADGET_LENGTH_DEFAULT, "minimum gadget length to include in fingerprint")
-    rootCmd.PersistentFlags().UintVarP(&gadgetSpec.MaxSize, "gadget-max-length", "l", MAX_GADGET_LENGTH_DEFAULT, "maximum gadget length to include in fingerprint")
 }
