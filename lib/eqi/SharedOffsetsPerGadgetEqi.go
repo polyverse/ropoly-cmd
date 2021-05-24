fde2ec81e3024c6e7269838b8fe06b4302dd4465
Thomas Oldham <toldham@uw.edu>
2021-05-24 07:15:55 -0700

    Refactor default EQI

--- lib/eqi/SharedOffsetsPerGadgetEqi.go
+++ lib/eqi/SharedOffsetsPerGadgetEqi.go
@@ -6,7 +6,6 @@ import (
 
 func SharedOffsetsPerGadgetEqi(f1, f2 Fingerprint.Fingerprint) float64 {
 	gadgetInstanceCount := 0
-	displacementsByGadgetInstance := make(map[uint64][]int64)
 	gadgetInstanceCountsByDisplacement := make(map[int64]uint)
 
 	for key, f1Addresses := range f1.Contents {
@@ -15,25 +14,26 @@ func SharedOffsetsPerGadgetEqi(f1, f2 Fingerprint.Fingerprint) float64 {
 		for _, f1Address := range f1Addresses {
 			for _, f2Address := range f2Addresses {
 				displacement := int64(f2Address) - int64(f1Address)
-				displacementsByGadgetInstance[f1Address] = append(displacementsByGadgetInstance[f1Address], displacement)
 				gadgetInstanceCountsByDisplacement[displacement] += 1
 			}
 		}
 	}
 
-	sharedCountSum := uint(0)
-	for _, f1Addresses := range f1.Contents {
-		for _, f1Address := range f1Addresses {
-			sharedCount := uint(0)
-			for _, displacement := range displacementsByGadgetInstance[f1Address] {
-				sharedCountCandidate := gadgetInstanceCountsByDisplacement[displacement]
-				if sharedCountCandidate > sharedCount {
-					sharedCount = sharedCountCandidate
-				}
-			}
-			sharedCountSum += sharedCount
-		}
-	}
+    sharedCountSum := uint(0)
+    for key, f1Addresses := range f1.Contents {
+        f2Addresses := f2.Contents[key]
+        for _, f1Address := range f1Addresses {
+            sharedCount := uint(0)
+            for _, f2Address := range f2Addresses {
+                displacement := int64(f2Address) - int64(f1Address)
+                sharedCountCandidate := gadgetInstanceCountsByDisplacement[displacement]
+                if sharedCountCandidate > sharedCount {
+                    sharedCount = sharedCountCandidate
+                }
+            }
+            sharedCountSum += sharedCount
+        }
+    }
 	sharedProportionSum := float64(sharedCountSum) / float64(gadgetInstanceCount)
 	sharedProportionMean := sharedProportionSum / float64(gadgetInstanceCount)
 	return (float64(1.0) - sharedProportionMean) * float64(100.0)
